b4f3238bc0e5a50e6a504b50ef50ee191f0801a4
PopOff <nythblox@gmail.com>
2025-10-16 19:17:28 -0700

    only does it to in-game store items, so it doesn't interfer with scripts

--- main.lua
+++ main.lua
@@ -1,7 +1,25 @@
+local whitelisteditems = {
+'Water',
+'Bloxy Cola',
+'Cheese Burger',
+'Coffee',
+'Orange Juice',
+'Pizza',
+'Chocolate Milk',
+'cheese',
+'Sandwich',
+'Cake',
+'Taco',
+'Apple',
+'toy',
+'IceCream Cone',
+'Candy Bucket',
+'Ghost Burger',
+}
 game:GetService('RunService').RenderStepped:Connect(function()
 local char = game.Players.LocalPlayer.Character
 for _,v in ipairs(char:GetChildren()) do
-if v:IsA("Tool") then
+if v:IsA("Tool") and (not table.find({whitelisteditems}, v.Name) then
 v.Parent = game.Players.LocalPlayer.Backpack
 end
 end
