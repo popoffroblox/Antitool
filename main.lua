15138f16732f6dc11969b0abf606fc342e04ceb5
PopOff <nythblox@gmail.com>
2025-10-16 19:23:57 -0700

    updated so it only checks for in-game shop items, and not to interfere with other running scripts using tools

--- main.lua
+++ main.lua
@@ -1,27 +1,26 @@
-local whitelisteditems = {
-'Water',
-'Bloxy Cola',
-'Cheese Burger',
-'Coffee',
-'Orange Juice',
-'Pizza',
-'Chocolate Milk',
-'cheese',
-'Sandwich',
-'Cake',
-'Taco',
-'Apple',
-'toy',
-'IceCream Cone',
-'Candy Bucket',
-'Ghost Burger',
-}
-game:GetService('RunService').RenderStepped:Connect(function()
-local char = game.Players.LocalPlayer.Character
-for _,v in ipairs(char:GetChildren()) do
-if v:IsA("Tool") and (not table.find({whitelisteditems}, v.Name) then
-v.Parent = game.Players.LocalPlayer.Backpack
-end
-end
+game:GetService("RunService").RenderStepped:Connect(function()
+    local char = game.Players.LocalPlayer.Character
+    for _, v in ipairs(char:GetChildren()) do
+        if v:IsA("Tool") and table.find({
+            "Water",
+            "Bloxy Cola",
+            "Cheese Burger",
+            "Coffee",
+            "Orange Juice",
+            "Pizza",
+            "Chocolate Milk",
+            "cheese",
+            "Sandwich",
+            "Cake",
+            "Taco",
+            "Apple",
+            "toy",
+            "IceCream Cone",
+            "Candy Bucket",
+            "Ghost Burger",
+        }, v.Name) then
+            v.Parent = game.Players.LocalPlayer.Backpack
+        end
+    end
 end)
 game.TextChatService.TextChannels.RBXGeneral:SendAsync('AntiTooler Loaded! [Made by popoff]')
